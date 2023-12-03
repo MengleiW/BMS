180a29cd6b714a156927fd0204d30713724bb686
MengleiW <149352448+MengleiW@users.noreply.github.com>
2023-12-03 17:32:54 -0600

    Update Bridge Sampling.py

--- Bridge Sampling.py
+++ Bridge Sampling.py
@@ -4,15 +4,71 @@ This file provides a single function that uses the RIS to find initial Rhat and 
 
 import numpy as np
 import matplotlib.pyplot as plt
-import math
 import pickle
 import scipy.special
 import BMS
-import random
 
 
 
+def Metropolis_hasting(weights,M,m,target_function,proposal_function ):
+    """
+    Description: Use the Metropolis-Hastings sampler to generate a sample from a Rayleigh distribution.
 
+    Inputs:
+       
+        sigma: double >0, the standard deviation of Multivariate normal
+               distribution representing the experimental error.
+       
+        m: the dimension of the variables.
+        
+        M: number of samples 
+        
+        Target_function:   Fucntion of distribution that we wish to sample from. 
+        
+        proposal_function: Function that we propose for candidates.
+        
+       
+    Outputs:
+   
+         Theta1: Sample gathered from the target distribution.
+         Theta2: Sample gathers from the target distribution.
+       
+       
+     Modified:
+   
+         11/09/2023 (Menglei Wang)
+    """ 
+
+    #Set empty parameters
+    theta1 = []
+    theta2 = []
+    X_t = np.ones(2*m)
+    
+    #if proposal_function == 'Gaussian':
+        #proposal_function = x: np.random.multivariate_normal(x, cov=np.eye(len(x)) * 0.1)
+
+    for i in range(M):
+        # Propose a new state from multivariate distribution 
+        Y = proposal_function(X_t)
+        Y1, Y2 = Y[:m], Y[m:]
+        #calculate acceptance rate alpha ratio, reduction due to symmetric proposal distributions.
+        r = target_function(Y)/target_function(X_t) #* weights
+       # print('r=',r)
+        
+        alpha = np.minimum(1, r)
+        #print('alpha=',alpha)
+        
+        if np.random.random() < alpha:
+            X_t = Y
+            theta1.append(Y1)
+            theta2.append(Y2)
+        else:
+            
+            theta1.append(X_t[:m])
+            theta2.append(X_t[m:])
+    theta1 = np.array([arr.flatten() for arr in theta1])
+    theta2 = np.array([arr.flatten() for arr in theta2])
+    return theta1, theta2
 
 
 def OptimalBridge (data,sigma, NS):
@@ -48,8 +104,12 @@ def OptimalBridge (data,sigma, NS):
     y1 = data['model01']['y']
     y2 = data['model02']['y']
     theta_bounds = np.array([[0.7,1.3],[0.7,1.3]])
+    N, m = tht2.shape
     N1,D1 = tht1.shape
     N2,D2 = tht1.shape
+    target_function = lambda x : scipy.stats.multivariate_normal.logpdf(x, cov=np.eye(len(x)) * 0.1)
+    
+    proposal_function  = lambda x: np.random.multivariate_normal(x, cov=np.eye(len(x)) * 0.1)
     
     #Finding Q11
     prior1 = BMS.uniform_prior(tht1, theta_bounds)
@@ -68,38 +128,84 @@ def OptimalBridge (data,sigma, NS):
     q21 = np.array(prior1*likelihood2)
     
     #Caculating the initial rhat
-    rhat = BMS.RisModel_compaire(data, sigma)
+    rhat =  1/BMS.RisModel_compaire(data, sigma)
+    print('rhat=', rhat)
+    
+    #Update q3
+    
+    weights = np.logaddexp.reduce(q12*q22) - np.logaddexp.reduce(N1*q12 - N2*rhat*q22)
+    
     
     for i in range (NS):
+         #Taking sampling using Metropolis Hasting algrithm. 
+         tht1, tht2 = Metropolis_hasting(weights,N,m,target_function,proposal_function )
          
-         Q1 = (np.sum(q12/(N1*q12+N2*rhat*q22)))/N2
-         Q2 = (np.sum(q21/(N1*q11+N2*rhat*q21)))/N1
-         print('q1=',rhat)
-         rhat = Q1/Q2
-
-    h = rhat +1
-    Z2 = rhat/h
-    Z1 = 1/h
+         
+         #Finding Q11
+         prior1 = BMS.uniform_prior(tht1, theta_bounds)
+         likelihood1 = BMS.gauss_log_likelihood(y1,sigma)
+         q11 = np.array(prior1*likelihood1)
+         #print('l1=',likelihood1)
+         
+         #Finding Q22
+         prior2 = BMS.uniform_prior(tht2, theta_bounds)
+         likelihood2 = BMS.gauss_log_likelihood(y2,sigma)
+         q22 = np.array(prior2*likelihood2)
+         #print('l2=',likelihood2)
+         
+         #Finding Q12
+         q12 = np.array(prior2*likelihood1)
+         #print('q12=',q12)
+         
+         #Finding Q21
+         q21 = np.array(prior1*likelihood2) 
+         #print('q21=',q21)
+        
+         epsilon = 1e-10
+         q11 = np.maximum(q11, epsilon)
+         q12 = np.maximum(q12, epsilon)
+         q21 = np.maximum(q21, epsilon)
+         q22 = np.maximum(q22, epsilon)
+         
+         
+         Q1 = np.logaddexp.reduce(np.log(q12)) - np.logaddexp.reduce(np.log(N1*q12 + N2*rhat*q22))
+         Q2 = np.logaddexp.reduce(np.log(q21)) - np.logaddexp.reduce(np.log(N1*q11 + N2*rhat*q21))
+         print('Q1=',Q1)
+         print('Q2=',Q2)
+         
+         
+         
+         
+         
+         rhat = np.exp(Q1 - Q2)
+         #Update q3
+         
+         weights = np.logaddexp.reduce(q12*q22) - np.logaddexp.reduce(N1*q12 - N2*rhat*q22)
+         #print('weights=',weights)
         
     #graphing
-    plt.pie([Z2,Z1], labels= [Q2,Q1], colors = ["red","blue"])
+    Q1 = np.sum(np.exp(q11))
+    Q2 = np.sum(np.exp(q22))
+    print('Q1=',Q1)
+    print('Q2=',Q2)
+    print('rhat=', rhat)
+    
+    plt.pie([Q2,Q1], labels= [Q2,Q1], colors = ["red","blue"],autopct='%1.1f%%')
     plt.annotate('Red is Z2', xy=(-1.1,0.8))
     plt.annotate('Blue is Z1', xy=(-1.1, 0.9)) 
     plt.title('Rhat')
     plt.tight_layout()
     plt.show()
 
-
     return rhat
 
 
 if __name__ == '__main__':
-    atomic_data_pickle_path = 'C:\\Users\\whisk\\atomic_data.pickle'
-    atomic_data_path = '/home/hans-werner/Dropbox/work/research/projects/bayesian_model_selection/data/atomic_data.pickle'
+    atomic_data_path = 'C:\\Users\\whisk\\atomic_data.pickle'
     #atomic_data_path =  '../data/atomic_data.pickle'
-    with open(atomic_data_path, 'rb')  as f:
+    with open('atomic_data.pickle', 'rb')  as f:
        data = pickle.load(f)
-    NS = 10   
+    NS = 10  
     sigma = 0.1
     #HMModel_compaire (data,sigma)
     #RisModel_compaire (data,sigma)
@@ -110,11 +216,4 @@ if __name__ == '__main__':
     tht2 = data['model02']['tht']
     
     OptimalBridge (data,sigma, NS)
-    
-    
-    
-    
-    
-    
-    
     
